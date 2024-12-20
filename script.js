67f0eab1beabf241cbdbd6edfae4ad0fddf4f3b2
Wendeldddd <166327243+Wendeldddd@users.noreply.github.com>
2024-12-20 14:47:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.getElementById('curso1').addEventListener('click', openWindow);
 
-function openWindow() {
-    document.getElementById('curso1Window').style.display = 'block';
-}
-
-function closeWindow() {
-    document.getElementById('curso1Window').style.display = 'none';
-}
