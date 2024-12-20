85d1f64364a03731baa450f2b87e962d45c50f49
Wendeldddd <166327243+Wendeldddd@users.noreply.github.com>
2024-12-20 11:40:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('curso1').addEventListener('click', openWindow);
+
+function openWindow() {
+    document.getElementById('curso1Window').style.display = 'block';
+}
+
+function closeWindow() {
+    document.getElementById('curso1Window').style.display = 'none';
+}
